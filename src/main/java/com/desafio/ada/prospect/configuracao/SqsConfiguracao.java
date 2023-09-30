f6fbd8a627df57556eefa797d31fd3effe56ee58
Matheus <matheus_adps@hotmail.com>
2023-09-29 22:52:30 -0300

    F8_SQS# implementação do serviço SQS da AWS

---
+++ src/main/java/com/desafio/ada/prospect/configuracao/SqsConfiguracao.java
@@ -0,0 +1,41 @@
+package com.desafio.ada.prospect.configuracao;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
+import io.awspring.cloud.messaging.core.QueueMessagingTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class SqsConfiguracao {
+
+    @Value("${spring.cloud.aws.region.static}")
+    private String region;
+    @Value("${spring.cloud.aws.credentials.access-key}")
+    private String accessKey;
+    @Value("${spring.cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public QueueMessagingTemplate queueMessagingTemplate()
+    {
+        return new QueueMessagingTemplate(amazonSQSAsync());
+    }
+
+    @Bean
+    @Primary
+    public AmazonSQSAsync amazonSQSAsync()
+    {
+        return AmazonSQSAsyncClientBuilder
+                .standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(
+                        new BasicAWSCredentials(accessKey, secretKey)))
+                .build();
+    }
+
+}
--- src/main/java/com/desafio/ada/prospect/pessoa/fisica/PessoaFisica.java
+++ src/main/java/com/desafio/ada/prospect/pessoa/fisica/PessoaFisica.java
@@ -3,12 +3,14 @@ package com.desafio.ada.prospect.pessoa.fisica;
 import com.desafio.ada.prospect.pessoa.Pessoa;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.ToString;
 
 import java.util.UUID;
 
+@ToString
 @AllArgsConstructor
 @Entity
-public class PessoaFisica extends Pessoa {
+public class PessoaFisica extends Pessoa  {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
